a204ece05cde987214a588066baf19a655fe9e94
loveloliii <woshigegefengleng@live.com>
2019-02-20 19:08:40 +0800

    更新文件上传代码
    Signed-off-by: loveloliii <woshigegefengleng@live.com>

--- src/main/java/com/panacealab/panacare/controller/RepairController.java
+++ src/main/java/com/panacealab/panacare/controller/RepairController.java
@@ -5,6 +5,8 @@ import com.panacealab.panacare.service.RepairService;
 import com.panacealab.panacare.utils.FileUtil;
 import com.panacealab.panacare.utils.StateCode;
 import com.panacealab.panacare.utils.TokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +26,7 @@ public class RepairController {
     @Autowired
     private IRedisService iRedisService;
 
-
+    private Logger logger = LoggerFactory.getLogger("RepairController");
     @RequestMapping(path = "submitRepairMsg", method = RequestMethod.POST)
     private String submitRepairMsg(HttpServletRequest httpServletRequest, @RequestParam(value = "token", required = false) String token) {
         String  code = TokenUtil.getTokenValues(token);
@@ -35,6 +38,23 @@ public class RepairController {
         String rs = repairService.submitRepairMsg(fileNameList, user_uniq_id);
         return rs;
     }
+    @RequestMapping(path = "uploadSingleFile", method = RequestMethod.POST)
+    private Map uploadSingleFile(HttpServletRequest httpServletRequest){
+        logger.info("\n进入uploadSingleFile方法");
+        List<Map> fileNameList;
+        fileNameList = FileUtil.saveFile(httpServletRequest);
+        Map<String,Object> resultMap = new HashMap<>(16);
+        resultMap.put("state", StateCode.Initial_Code);
+        //验证token
+      /*  String token = (String) fileNameList.get("token");
+        String rs = TokenUtil.checkLoginState(token);
+        if (!StateCode.Initial_Code.equals(rs)) {
+            resultMap.put("state", rs);
+            return resultMap;
+        }*/
 
+        //String rs2 = repairService.saveFileInfo(file);
+        return resultMap;
+    }
 
 }
