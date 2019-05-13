f99b492dbe6a8659a3b2f84a94b5f28dcce10490
loveloliii <woshigegefengleng@live.com>
2019-05-13 17:25:05 +0800

    邮件订阅相关
    Signed-off-by: loveloliii <woshigegefengleng@live.com>

---
+++ src/main/java/com/panacealab/panacare/controller/EmailSubscribeController.java
@@ -0,0 +1,13 @@
+package com.panacealab.panacare.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * email related
+ * @author loveloliii
+ * @date 2019/5/13.
+ */
+@RestController
+public class EmailSubscribeController {
+
+}
